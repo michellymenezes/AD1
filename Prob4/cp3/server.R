9d0c1d3e3bb55078cb5ae546f95b7b494a9c7bef
Michelly Menezes <martha.michelly@gmail.com>
2016-09-23 03:58:13 -0300

    user interaction

--- Prob4/cp3/server.R
+++ Prob4/cp3/server.R
@@ -36,55 +36,135 @@ moviegenre = data %>%
 
 genres = unique(moviegenre$genre)
 moviegenre$year = as.numeric( substrYear(moviegenre$title))
+decades = c(1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010)
+
 
 moviesdates = moviegenre[!duplicated(moviegenre[,c('movieId')]),]
 moviesdates$decade = NA
-
-decades = c(1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010)
-
 n = -90
 for (i in 1:11){
   moviesdates$decade[moviesdates$year-2000 >= n] = decades[i]
-  ano = ano + 10
   n = n + 10
 }
-
 moviesdates = na.omit(moviesdates)
 
 
+geral.df <- data.frame()
+for(i in decades){
+  b = bootstrap(subset(moviesdates, decade==i) , median(rating))
+  median.geral = CI.percentile(b, probs = c(.025, .975))
+  geral.df <- data.frame(rbind(geral.df, data.frame(median.geral)))
+}
+geral.df$decade = decades
 
+
+adventure = subset(moviegenre, moviegenre$genre == "Adventure")
+adventure$decade = NA
+n = -90
+for (i in 1:11){
+  adventure$decade[adventure$year-2000 >= n] = decades[i]
+  n = n + 10
+}
+adventure = na.omit(adventure)
+
+
+adventure.df <- data.frame()
+vector <- c()
+for(i in decades){
+  if(nrow(subset(adventure, decade == i)) >= 30){
+    b = bootstrap(subset(adventure, decade==i) , median(rating))
+    median.adventure = CI.percentile(b, probs = c(.025, .975))
+    adventure.df <- data.frame(rbind(adventure.df, data.frame(median.adventure)))
+    vector <- c(vector, i)
+  }
+}
+adventure.df$decade = vector
+
+##########################################
 shinyServer(function(input, output) {
-  output$plot0 <- renderPlot({
+  output$plot0 <- renderPlotly({
     
-    p = ggplot( moviesdates, aes(decade , rating)) + 
+    p = ggplot( moviesdates, aes(year , rating)) + 
+      scale_x_continuous(breaks = pretty(geral.df$decade, n = 10)) +
       geom_point(position = position_jitter(width = .1), alpha = .4)
     
-    print(p)
+    (gg <- ggplotly(p))
   })
     
-  
-  
+  #############################################
+
     output$plot1 <- renderPlot({
-    df <- data.frame()
-    
-    for(i in decades){
-      b = bootstrap(subset(moviesdates, decade==i) , median(rating))
-      median.jp = CI.percentile(b, probs = c(.025, .975))
-      df <- data.frame(rbind(df, data.frame(median.jp)))
-    }
-    
-    
-    df$decade = decades
-    
-    q = df %>% 
+     q = geral.df %>% 
       ggplot(aes(x = decade, ymin = X2.5., ymax = X97.5.)) +
       geom_errorbar(width = 5) +
-      scale_x_continuous(breaks = pretty(df$decade, n = 10)) +
+      scale_x_continuous(breaks = pretty(geral.df$decade, n = 10)) +
       labs(x="Ano do filme", y="Mediana")
-      
-    
+
     print(q)
     
+  })
+
+##########################
+    output$plot2 <- renderPlotly({
+
+      r = ggplot( adventure, aes(year , rating)) +
+        scale_x_continuous(breaks = pretty(adventure$decade, n = 10)) +
+        geom_point(position = position_jitter(width = .1), alpha = .4)
+
+      (gg <- ggplotly(r))
+    })
+########################
+    output$plot3 <- renderPlot({
+
+      s = adventure.df %>%
+        ggplot(aes(x = decade, ymin = X2.5., ymax = X97.5.)) +
+        geom_errorbar(width = 5) +
+        scale_x_continuous(breaks = pretty(adventure.df$decade, n = 10)) +
+        labs(x="Ano do filme", y="Mediana")
+
+      print(s)
+    })
+#######################################    
+  output$plot4 <- renderPlotly({
+    
+    genreN = subset(moviegenre, moviegenre$genre == input$selectGenre0)
+    genreN$decade = NA
+    n = -90
+    for (i in 1:11){
+      genreN$decade[genreN$year-2000 >= n] = decades[i]
+      n = n + 10
+    }
+    genreN = na.omit(genreN)
+    
+    p4 <- ggplot( genreN, aes(year , rating)) +
+      scale_x_continuous(breaks = pretty(genreN$decade, n = 10)) +
+      geom_point(position = position_jitter(width = .1), alpha = .4)
+    (gg <- ggplotly(p4))
+  })
+  #####################################
+  output$plot5 <- renderPlot({
+    
+    genreUp = input$selectGenre0
+    
+    genreN.df <- data.frame()
+    vectorN <- c()
+    for(i in decades){
+      if(nrow(subset(genreN, decade == i)) >= 30){
+        b = bootstrap(subset(genreN, decade==i) , median(rating))
+        median.genreN = CI.percentile(b, probs = c(.025, .975))
+        genreN.df <- data.frame(rbind(genreN.df, data.frame(median.genreN)))
+        vectorN <- c(vectorN, i)
+      }
+    }
+    genreN.df$decade = vectorN
+    
+    outputN = genreN.df %>%
+      ggplot(aes(x = decade, ymin = X2.5., ymax = X97.5.)) +
+      geom_errorbar(width = 5) +
+      scale_x_continuous(breaks = pretty(genreN.df$decade, n = 10)) +
+      labs(x="Ano do filme", y="Mediana")
+    
+    print(outputN)
   })
   
 })
--- Prob4/cp3/ui.R
+++ Prob4/cp3/ui.R
@@ -12,17 +12,37 @@ library(plotly)
 
 
 shinyUI(fluidPage(
+  
   titlePanel("Checkpoint 3"),
   
-  sidebarLayout(
-    sidebarPanel(width = 3,
-                 helpText()),
-    
-    mainPanel(width = 9,
-              h2("Filmes"),
-              plotOutput(outputId = "plot0"),
-              plotOutput(outputId = "plot1")
-            )
-    
-  )
+
+    navlistPanel(tabPanel("Apresentação",
+                          h3("Apresentação"),
+                          "Está é uma análise relacionada a filmes, juntamente com seus gêneros e avaliações de usuários."),
+                 "Gêrenos ao longo de anos",
+                 tabPanel("Geral",
+                          h3("Geral"),
+                          h4("Como filmes evoluem ao longo do tempo com relação a notas em avaliações?"),
+                          fluidRow(plotlyOutput(outputId = "plot0")),
+                          fluidRow(plotOutput(outputId = "plot1"))
+                          ),
+                 tabPanel("Uma análise",
+                          h3("Uma análise"),
+                          h4("Se resolvermos escolher um gênero para analisar a mesma evolução, qual o comportamente resultante? Existe diferença para a abordagem com todos s gêneros?"),
+                          fluidRow(plotlyOutput(outputId = "plot2")),
+                          fluidRow(plotOutput(outputId = "plot3"))
+                          ),
+                 tabPanel("Sua vez",
+                          selectInput("selectGenre0", label = h3("Escolha um gênero:"), 
+                                      choices = list("Adventure", "Animation", "Children", "Comedy", "Fantasy", "Romance", "Drama", "Action", "Crime", "Thriller",
+                                                     "Horror", "Mystery", "Sci-Fi", "War", "Musical", "Documentary", "IMAX", "Western", "Film-Noir"), 
+                                      selected = "Adventure"),
+                          fluidRow(plotlyOutput(outputId = "plot4")),
+                          fluidRow(plotOutput(outputId = "plot5"))
+                          ),
+                 "Gênero + Gêneros",
+                 tabPanel("Geral"),
+                 tabPanel("Uma análise"),
+                 tabPanel("Sua vez")
+    )
 ))
