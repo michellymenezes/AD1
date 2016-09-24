3f6f425a231b48e86ebb6602cc5acc496256f026
Michelly Menezes <martha.michelly@gmail.com>
2016-09-24 04:23:53 -0300

    intro e parte 1

--- Prob4/cp3/server.R
+++ Prob4/cp3/server.R
@@ -1,12 +1,5 @@
 # server.R
 
-# Para filmes com um determinado gênero, como eles evoluem ao longo do tempo 
-# com relação a notas em avaliações?
-
-# Para filmes com mais de um gênero, se fixarmos um gênero, quais gêneros combinados
-# com o fixado rendem melhores avaliações?
-
-
 library(shiny)
 library(plotly)
 library(readr)
@@ -17,57 +10,28 @@ library(stringr)
 
 data <- read.csv("data/ratings-por-filme.csv")
 
-l2w_genres = function(line){
-  resposta = rep(line, times = 1)
-  g = data.frame(genre = unlist(strsplit(as.character(line$genres), '[|]')))
-  g$title = line$title
-  g$year = as.numeric(substrYear(line$title))
-  return(full_join(as.data.frame(line), g))
-}
+# l2w_genres = function(line){
+#   resposta = rep(line, times = 1)
+#   g = data.frame(genre = unlist(strsplit(as.character(line$genres), '[|]')))
+#   g$title = line$title
+#   g$year = as.numeric(substrYear(line$title))
+#   return(full_join(as.data.frame(line), g))
+# }
 
-substrYear <- function(titulo){
-  return(substr(as.character( titulo), nchar(as.character(titulo))-4, nchar(as.character(titulo)) - 1))
-}
+# substrYear <- function(titulo){
+#   return(substr(as.character( titulo), nchar(as.character(titulo))-4, nchar(as.character(titulo)) - 1))
+# }
 
-moviegenre = data %>%
-  #select(title, genres) %>%
-  rowwise() %>%
-  do(l2w_genres(.))
+# moviegenre = data %>%
+#   #select(title, genres) %>%
+#   rowwise() %>%
+#   do(l2w_genres(.))
+
+moviegenre = read.csv("data/movie_genres.csv")
 
 genres = unique(moviegenre$genre)
-moviegenre$year = as.numeric( substrYear(moviegenre$title))
-data$year = as.numeric( substrYear(data$title))
 
 decades = c(1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010)
-
-moviegenre$decade = NA
-data$decade = NA
-
-
-moviesdates = moviegenre[!duplicated(moviegenre[,c('movieId')]),]
-n = -90
-for (i in 1:11){
-  moviesdates$decade[moviesdates$year-2000 >= n] = decades[i]
-  moviegenre$decade[moviegenre$year-2000 >= n] = decades[i]
-  data$decade[data$year-2000 >= n] = decades[i]
-  
-  
-  n = n + 10
-}
-moviesdates = na.omit(moviesdates)
-moviegenre = na.omit(moviegenre)
-data = na.omit(data)
-
-
-
-geral.df <- data.frame()
-for(i in decades){
-  b = bootstrap(subset(moviesdates, decade==i) , median(rating))
-  median.geral = CI.percentile(b, probs = c(.025, .975))
-  geral.df <- data.frame(rbind(geral.df, data.frame(median.geral)))
-}
-geral.df$decade = decades
-
 
 adventure = subset(moviegenre, moviegenre$genre == "Adventure")
 adventure$decade = NA
@@ -83,7 +47,7 @@ adventure.df <- data.frame()
 vector <- c()
 for(i in decades){
   if(nrow(subset(adventure, decade == i)) >= 30){
-    b = bootstrap(subset(adventure, decade==i) , median(rating))
+    b = bootstrap(subset(adventure, decade==i) , mean(rating))
     median.adventure = CI.percentile(b, probs = c(.025, .975))
     adventure.df <- data.frame(rbind(adventure.df, data.frame(median.adventure)))
     vector <- c(vector, i)
@@ -91,31 +55,21 @@ for(i in decades){
 }
 adventure.df$decade = vector
 
+popAdventure.df <- data.frame()
+popVector <- c()
+for(i in decades){
+  if(nrow(subset(adventure, decade == i)) >= 30){
+    b = bootstrap(subset(adventure, decade==i) , mean(popularity))
+    median.adventure = CI.percentile(b, probs = c(.025, .975))
+    popAdventure.df <- data.frame(rbind(popAdventure.df, data.frame(median.adventure)))
+    popVector <- c(popVector, i)
+  }
+}
+popAdventure.df$decade = popVector
+
 ##########################################
 shinyServer(function(input, output) {
-  output$plot0 <- renderPlotly({
-    
-    p = ggplot( moviesdates, aes(year , rating)) + 
-      scale_x_continuous(breaks = pretty(geral.df$decade, n = 10)) +
-      geom_point(position = position_jitter(width = .1), alpha = .4)
-    
-    (gg <- ggplotly(p))
-  })
-    
-  #############################################
 
-    output$plot1 <- renderPlot({
-     q = geral.df %>% 
-      ggplot(aes(x = decade, ymin = X2.5., ymax = X97.5.)) +
-      geom_errorbar(width = 5) +
-      scale_x_continuous(breaks = pretty(geral.df$decade, n = 10)) +
-      labs(x="Ano do filme", y="Mediana")
-
-    print(q)
-    
-  })
-
-##########################
     output$plot2 <- renderPlotly({
 
       r = ggplot( adventure, aes(year , rating)) +
@@ -131,10 +85,23 @@ shinyServer(function(input, output) {
         ggplot(aes(x = decade, ymin = X2.5., ymax = X97.5.)) +
         geom_errorbar(width = 5) +
         scale_x_continuous(breaks = pretty(adventure.df$decade, n = 10)) +
-        labs(x="Ano do filme", y="Mediana")
+        labs(x="Década do filme", y="Mediana")
 
       print(s)
     })
+    
+    ########################
+    output$plot0 <- renderPlot({
+      
+      pop = popAdventure.df %>%
+        ggplot(aes(x = decade, ymin = X2.5., ymax = X97.5.)) +
+        geom_errorbar(width = 5) +
+        scale_x_continuous(breaks = pretty(popAdventure.df$decade, n = 10)) +
+        labs(x="Década do filme", y="Popularidade")
+      
+      print(pop)
+    })
+    
 #######################################    
   output$plot4 <- renderPlotly({
     
@@ -155,13 +122,20 @@ shinyServer(function(input, output) {
   #####################################
   output$plot5 <- renderPlot({
     
-    genreUp = input$selectGenre0
-    
+    genreN = subset(moviegenre, moviegenre$genre == input$selectGenre0)
+    genreN$decade = NA
+    n = -90
+    for (i in 1:11){
+      genreN$decade[genreN$year-2000 >= n] = decades[i]
+      n = n + 10
+    }
+    genreN = na.omit(genreN)
+
     genreN.df <- data.frame()
     vectorN <- c()
     for(i in decades){
       if(nrow(subset(genreN, decade == i)) >= 30){
-        b = bootstrap(subset(genreN, decade==i) , median(rating))
+        b = bootstrap(subset(genreN, decade==i) , mean(rating))
         median.genreN = CI.percentile(b, probs = c(.025, .975))
         genreN.df <- data.frame(rbind(genreN.df, data.frame(median.genreN)))
         vectorN <- c(vectorN, i)
@@ -173,7 +147,7 @@ shinyServer(function(input, output) {
       ggplot(aes(x = decade, ymin = X2.5., ymax = X97.5.)) +
       geom_errorbar(width = 5) +
       scale_x_continuous(breaks = pretty(genreN.df$decade, n = 10)) +
-      labs(x="Ano do filme", y="Mediana")
+      labs(x="Década do filme", y="Mediana")
     
     print(outputN)
   })
@@ -184,27 +158,61 @@ shinyServer(function(input, output) {
   output$plot6 <- renderPlot({
     
     combG = subset(combGenres, grepl(input$selectGenre1, combGenres$genres))
-    #combG2 = subset(combG, grepl(input$selectGenre2, combG$genres))
-    
+
     combG.df <- data.frame()
     vectorCombG <- c()
     for(i in genres){
-      if(nrow(subset(combG, grepl(i, combG$genres))) >= 30){
-        b = bootstrap(subset(combG, grepl(i, combG$genres)) , median(rating))
+      # if(nrow(subset(combG, 
+      #                (grepl(i, combG$genres) &
+      #                 str_count(combG$genres, "[|]") >= input$selectN0 &
+      #                  str_count(combG$genres, "[|]") <= input$selectN1)
+      #                )
+      #         ) > 30){
+      if(nrow(subset(combG, grepl(i, combG$genres))) >= 30 & i != input$selectGenre1){
+        b = bootstrap(subset(combG, grepl(i, combG$genres)) , mean(rating))
         median.combG = CI.percentile(b, probs = c(.025, .975))
         combG.df <- data.frame(rbind(combG.df, data.frame(median.combG)))
         vectorCombG <- c(vectorCombG, i)
       }
     }
-    combG.df$Gender = vectorCombG
+    combG.df$gender = vectorCombG
     
     outputGenderN = combG.df %>%
-      ggplot(aes(x = Gender, ymin = X2.5., ymax = X97.5.)) +
+      ggplot(aes(x = gender, ymin = X2.5., ymax = X97.5.)) +
       geom_errorbar(width = 0.5) +
       labs(x="Gênero", y="Mediana")
     
     print(outputGenderN)
   })
   
+  output$plot7 <- renderPlot({
+    
+    advG = subset(combGenres, grepl("Adventure", combGenres$genres))
+
+    advG.df <- data.frame()
+    vectorAdvG <- c()
+    for(i in genres){
+      # if(nrow(subset(combG, 
+      #                (grepl(i, combG$genres) &
+      #                 str_count(combG$genres, "[|]") >= input$selectN0 &
+      #                  str_count(combG$genres, "[|]") <= input$selectN1)
+      #                )
+      #         ) > 30){
+      if(nrow(subset(advG, grepl(i, advG$genres))) >= 30 & i != "Adventure"){
+        b = bootstrap(subset(advG, grepl(i, advG$genres)) , mean(rating))
+        median.AdvG = CI.percentile(b, probs = c(.025, .975))
+        advG.df <- data.frame(rbind(advG.df, data.frame(median.AdvG)))
+        vectorAdvG <- c(vectorAdvG, i)
+      }
+    }
+    advG.df$gender = vectorAdvG
+    
+    outputAdvG = advG.df %>%
+      ggplot(aes(x = gender, ymin = X2.5., ymax = X97.5.)) +
+      geom_errorbar(width = 0.5) +
+      labs(x="Gênero", y="Mediana")
+    
+    print(outputAdvG)
+  })
   
 })
--- Prob4/cp3/ui.R
+++ Prob4/cp3/ui.R
@@ -1,55 +1,75 @@
 # ui.R
 
-# Para filmes com um determinado gênero, como eles evoluem ao longo do tempo 
-# com relação a notas em avaliações?
-
-# Para filmes com mais de um gênero, se fixarmos um gênero, quais gêneros combinados
-# com o fixado rendem melhores avaliações?
-
-
 library(shiny)
 library(plotly)
 library(shinythemes)
 
-
-
 shinyUI(fluidPage(
   
-  titlePanel("Checkpoint 3"),
+  titlePanel("Problema 4 - Checkpoint 3"),
   theme = shinytheme("flatly"),
 
     navlistPanel(tabPanel("Apresentação",
                           h3("Apresentação"),
-                          "Está é uma análise relacionada a filmes, juntamente com seus gêneros e avaliações de usuários."),
-                 "Gêrenos ao longo de anos",
-                 tabPanel("Geral",
-                          h3("Geral"),
-                          h4("Como filmes evoluem ao longo do tempo com relação a notas em avaliações?"),
-                          fluidRow(plotlyOutput(outputId = "plot0")),
-                          fluidRow(plotOutput(outputId = "plot1"))
+                          "Está é uma análise relacionada a filmes, juntamente com seus gêneros e avaliações de usuários. Cada filme foi avaliado por diversos usuários com notas que variam de 0.5 a 5. Além disso cada filme possui gêneros de classificação e ano de lançamento.",
+                          br(), br(),
+                          "O objetivo desse checkpoint é utilizar a técnica Bootstrap para o processo de reamostragem e, a partir desta, intepretar valores como variância e intervalo de confiança. Utilizamos reamostragem para inferir e tentar realizar afirmações sobre uma população a partir de uma amostra da mesma.",
+                          br(), br(),
+                          "Para esta análise, utilizamos o tempo dividido em décadas e os valores associados as avaliações são resultados de reamostragem das médias dos mesmos.",
+                          br(), br(),
+                          "Observação: esta é uma visualização com uma quantidade razoável de processamento. É muito provável que os gráficos demorem a carregar inicialmente ou logo após alterar um valor de entrada."
                           ),
+                 
+                 "Gêrenos ao longo de anos",
+
                  tabPanel("Uma análise",
                           h3("Uma análise"),
-                          h4("Se resolvermos escolher um gênero para analisar a mesma evolução, qual o comportamente resultante? Existe diferença para a abordagem com todos s gêneros?"),
+                          h4("Se resolvermos escolher um gênero para analisar como suas médias variam ao longo dos anos, qual o comportamente resultante?"),
+                          br(),
+                          "Para esta análise, vamos  escolher o gênero Adventure. A plotagem ao longo dos anos relacioda a este gênero pode ser observada no gráfico logo abaixo.",
                           fluidRow(plotlyOutput(outputId = "plot2")),
-                          fluidRow(plotOutput(outputId = "plot3"))
+                          br(),
+                          "Logo abaixo temos a representação do intervalo com confiança de 97.5%. A faixa tomada por cada intervalo é referente ao valor que a média de avaliações relacionadas ao gênero Adventure pode assumir para filmes lançados em cada década. Para que o processo de amostragem funcione corretamente precisamos de um número mínimo de elementos em cada amostra, então décadas com quantodades de filmes menores que 30 são desconsideradas e não incluídas na análise.",
+                          br(), br(),
+                          fluidRow(plotOutput(outputId = "plot3")),
+                          br(),
+                          "Observando os intervalos acima, podemos inferir que filmes da década de 50 possuem uma maior média de avaliação do que filmes da década de 90. E por que conseguimos interpretar desta forma? Porque não há interseção entre os intervalos de ambos e a faixa de valores dos filmes de 50's está acima dos de 90's. Isso significa que, para qualquer valor de média que os filmes dos anos 50 podem assumir, ele será sempre maior até mesmo do que o maior valor de média que filmes dos anos 90 podem assumir.",
+                          br(), br(),
+                          "Por outro lado não podemos afimar quando fazemos a mesma comparação para filmes de 2000 e 2010. Observando o gráfico, o máximo que podemos inferir é que existe a possibilidade de que filmes lançados em 2010 possuam uma maior média de avaliações que aqueles lançados em 2000. Isso acontece porque há inteserção de valores nos intervalos. Por mais que filmes de 2010 possam atingir maiores médias, não significa que irá ocorrer. Se filmes do anos 2000 atingirem sua possibilidade maior de média e os de 2010 de menor média, o cenário fica favorável a inferir que filmes lançados em 2000 possuem uma melhor média em avaliações que aqueles da década de 2010.",
+                          br(), br(),
+                          h4("Visto que temos os intervalos gerados para as médias de avaliações a cada década, será que a popularidade se comporta de maneira semelhante?"),
+                          br(), br(),
+                          "A popularidade de um filme é referente a quantidade de avaliações que o mesmo obteve. Aqui estaremos observando o quão popular filmes de um determinado gênero são em cada década.",
+                          fluidRow(plotOutput(outputId = "plot0"))
                           ),
-                 tabPanel("Sua vez",
-                          selectInput("selectGenre0", label = h3("Escolha um gênero:"), 
+                 
+                 tabPanel("Sua vez", h3("Sua vez"),
+                          br(),
+                          "Baseando-se na abordagem feita para a análise anterior, escolha um gênero, observe o gráfico gerado e tente fazer inferências a partir da análise dos intervalos gerados.",
+                          selectInput("selectGenre0", label = h4("Escolha um gênero:"), 
                                       choices = list("Adventure", "Animation", "Children", "Comedy", "Fantasy", "Romance", "Drama", "Action", "Crime", "Thriller",
                                                      "Horror", "Mystery", "Sci-Fi", "War", "Musical", "Documentary", "IMAX", "Western", "Film-Noir"), 
                                       selected = "Adventure"),
                           fluidRow(plotlyOutput(outputId = "plot4")),
                           fluidRow(plotOutput(outputId = "plot5"))
                           ),
+                 
                  "Gênero + Gêneros",
-                 tabPanel("Uma análise"),
-                 tabPanel("Sua vez",
-                          selectInput("selectGenre1", label = h3("Escolha um gênero:"), 
+                 
+                 tabPanel("Uma análise",
+                          h3("Uma análise"),
+                          h4("Se agora selecionamos um gênero, qual outro gênero combinado a ele possui melhores médias em avaliação?"),
+                          br(),
+                          "Desejamos escolher filmes que possuem uma combinação de gêneros em sua classificação e verificar o quão boas são essas combinações com base na média de avaliações de cada uma.
+                          Assim como na anterior, ara esta análise escolhemos o gênero Adventure. Abaixo podemos observar os intervalos gerados e divididos por gênero.",
+                          fluidRow(plotOutput(outputId = "plot7"))),
+                 
+                 tabPanel("Sua vez", h3("Sua vez"),
+                          selectInput("selectGenre1", label = h4("Escolha um gênero:"), 
                                       choices = list("Adventure", "Animation", "Children", "Comedy", "Fantasy", "Romance", "Drama", "Action", "Crime", "Thriller",
                                                      "Horror", "Mystery", "Sci-Fi", "War", "Musical", "Documentary", "IMAX", "Western", "Film-Noir"), 
                                       selected = "Adventure"),
-                          fluidRow(plotOutput(outputId = "plot6"))
+                           fluidRow(plotOutput(outputId = "plot6"))
                           
                           )
     )
